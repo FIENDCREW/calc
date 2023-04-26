ab092d764fde92ddfc456aea3f7b8c504cb00172
FIRNDCREW <odysseybmx911@gmail.com>
2023-04-26 21:17:06 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calc from "./Components/Calc";
+
+const App = () => {
+  return (
+    <>
+      <Calc />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Calc.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Calc = () => {
+  return (
+    <>Тут будет калькулятор</>
+    );
+};
+
+export default Calc;
